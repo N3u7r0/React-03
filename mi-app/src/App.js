c1a9c55a19db7405afd77de275d5ed69ad4d341a
N3u7r0 <brian_sabatini1993@hotmail.com>
2022-08-31 23:58:40 -0300

    nuevo commit

---
+++ mi-app/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Nav from './componentes/Nav';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Mi tienda online.com</h1>
+      <Nav />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ mi-app/src/componentes/Nav.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Nav = () => {
+    return (
+        <div className="navegador">
+            
+            <ul>
+                <li><h2>Mi tienda online.com</h2></li>
+                <li><a href="./public/index.html">Home</a></li>
+                <li><a href="./public/index.html">Contacto</a></li>
+                <li><a href="./public/index.html">Articulos</a></li>
+                <li><a href="./public/index.html">Ofertas</a></li>
+            </ul>
+        </div>
+    )
+
+}
+
+export default Nav;
